3b622dc93c75e8c29de14c805d93c475d8b4ec68
Tony Jian <tonytonyjan@gmail.com>
2014-07-02 12:35:36 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'guest_chat'
+set :application, 'guest-chat'
 set :repo_url, 'git@github.com:tonytonyjan/guest-chat.git'
 set :bundle_binstubs, false
 
