d6659f26b146bf2eff8e00e4cd55d415984d8355
Jian Weihang <tonytonyjan@gmail.com>
2014-11-27 00:10:45 +0800

    add dict link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,8 @@ module ApplicationHelper
       when 'student' then :primary
       end
     end
-    content_tag :span, message.guest.name, class: "name label label-#{type}"
+    match_data = message.guest.name.match(/(.{4})(.*)/)
+    name = link_to(match_data[1], "https://www.moedict.tw/#{u match_data[1]}", target: :_blank) + match_data[2]
+    content_tag :span, name, class: "name label label-#{type}"
   end
 end
