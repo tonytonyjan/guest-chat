147c0125a3ac6f3162e78a91e1f17310ea1b6740
Jian Weihang <tonytonyjan@gmail.com>
2016-09-06 03:26:50 +0800

    current guest

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_guest
+    @guest ||= (
+      guest = Guest.find_or_create_by!(id: session[:guest_id])
+      session[:guest_id] = guest.id
+      guest
+    )
+  end
+
+  helper_method :current_guest
 end
